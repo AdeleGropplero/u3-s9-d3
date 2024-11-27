07c0141f01cb59f8abc935c2129182be52385502
AdeleGropplero <adele.gdt@hotmail.com>
2024-11-27 16:39:49 +0100

    fine obbligatori inizio extra

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,24 +1,47 @@
+import { Component } from "react";
 import { Button, Card, Col } from "react-bootstrap";
 
-const SingleBook = (props) => {
-  return (
-    <Col xs={10} md={4} xl={3}>
-      <Card className="mb-4" style={{ height: "480px", borderRadius: "15px" }}>
-        <Card.Img variant="top" src={props.src} style={{ height: "350px" }} />
-        <Card.Body className="pb-2">
-          <Card.Title
-            className="text-truncate"
-            style={{ height: "45px", textOverflow: "ellipsis" }}
-          >
-            {props.title}
-          </Card.Title>
-          <Button className=" " variant="success">
-            + add
-          </Button>
-        </Card.Body>
-      </Card>
-    </Col>
-  );
-};
+class SingleBook extends Component {
+  state = {
+    selected: false
+  };
+
+  toggleSelected = () => {
+    console.log(this.state.selected);
+    this.setState({ selected: !this.state.selected });
+  };
+
+  render() {
+    const newClass = this.state.selected
+      ? "mb-4 border border-danger border-2"
+      : "mb-4";
+    return (
+      <Col xs={10} md={4} xl={3}>
+        <Card
+          onClick={this.toggleSelected}
+          className={newClass}
+          style={{ height: "480px" }}
+        >
+          <Card.Img
+            variant="top"
+            src={this.props.src}
+            style={{ height: "350px" }}
+          />
+          <Card.Body className="pb-2">
+            <Card.Title
+              className="text-truncate"
+              style={{ height: "45px", textOverflow: "ellipsis" }}
+            >
+              {this.props.title}
+            </Card.Title>
+            <Button className=" " variant="success">
+              + add
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  }
+}
 
 export default SingleBook;
